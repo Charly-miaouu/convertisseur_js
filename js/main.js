eb8fe65db8617b68cb385501edffaa1f846e378a
unknown <charles.belot@enigma-school.com>
2024-01-21 17:05:29 +0100

    need to modify buttons and font

--- js/main.js
+++ js/main.js
@@ -4,7 +4,30 @@ const outputUnit = document.getElementById('outputUnit');
 
 function convertUnits() {
     const result = performConversion(inputUnit.value, outputUnit.value, value.value);
-    document.getElementById('result').textContent = `Résultat : ${result}`;
+    const graphic = getEmoji(outputUnit.value);
+    document.getElementById('result').textContent =`Résultat : ${result}`;
+    for (let i = 0; i < result; i++)
+        document.getElementById('graphic').textContent += `${graphic}`;
+}
+
+function getEmoji(outputUnit) {
+    if (outputUnit == '330')
+        return '🗼';
+    else if (outputUnit == '1.756')
+        return '🧍🏽‍♂️';
+    else if (outputUnit == '56')
+        return '🚀';
+    else if (outputUnit == '0.1467')
+        return '📱';
+    else if (outputUnit == '1970')
+        return '🌉';
+    else if (outputUnit == '1')
+        return '👨🏻‍🏫';
+    else if (outputUnit == '1000')
+        return '🇰🇲';
+    else
+        return '🇨🇲';
+
 }
 
 function performConversion(inputUnit, outputUnit, value) {
